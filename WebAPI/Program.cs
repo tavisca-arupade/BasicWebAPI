90ec3eb5f88b56acb99517e41ab971a56dd1bec4
Aditi Rupade <arupade@affinionds.com>
2019-08-14 17:38:49 +0530

    changed bindings

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -19,6 +19,6 @@ namespace WebAPI
 
         public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
             WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+                .UseStartup<Startup>().UseUrls(urls:"http://*:10000");
     }
 }
