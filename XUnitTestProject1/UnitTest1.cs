fb45d662767181de854f48d794d77dd440ba2916
Aditi Rupade <arupade@affinionds.com>
2019-08-14 14:45:49 +0530

    Updated Test Cases

--- XUnitTestProject1/UnitTest1.cs
+++ XUnitTestProject1/UnitTest1.cs
@@ -1,14 +1,33 @@
 using System;
 using Xunit;
+using WebAPI.Controllers;
 
 namespace XUnitTestProject1
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void Test_default_greet_value()
         {
+            GreetingController greet = new GreetingController();
 
+            Assert.Equal("say helloo", greet.Get());
+        }
+
+        [Fact]
+        public void Test_greet_value_when_hello()
+        {
+            GreetingController greet = new GreetingController();
+
+            Assert.Equal("hi", greet.Get("hello").Value);
+        }
+
+        [Fact]
+        public void Test_greet_value_when_hi()
+        {
+            GreetingController greet = new GreetingController();
+
+            Assert.Equal("hello", greet.Get("hi").Value);
         }
     }
 }
